6bc5d845daf4dbe951e4e0bbae8750e3056907b1
Stilllee <orodae@gmail.com>
2023-10-15 21:21:25 +0900

    ♻️ Nav 컴포넌트 분리

--- src/routes/LandingPage.tsx
+++ src/routes/LandingPage.tsx
@@ -4,6 +4,7 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import useGoto from "../hooks/useGoto";
 import { device } from "../constants/breakpoints";
+import Nav from "../components/LandingPage/Nav";
 
 const Wrapper = styled.main`
   width: 100%;
@@ -194,51 +195,6 @@ const LoginBtn = styled(Button)`
   color: ${({ theme }) => theme.brown};
 `;
 
-const Nav = styled.nav`
-  width: 100%;
-  padding: 16px 20px;
-`;
-
-const NavList = styled.ul`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 16px;
-  flex-wrap: wrap;
-`;
-
-const NavItem = styled.li`
-  white-space: nowrap;
-  cursor: pointer;
-  font-size: 13px;
-  color: ${({ theme }) => theme.darkGray};
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const NavItems = [
-  "소개",
-  "PeaNutter 앱 다운로드하기",
-  "고객센터",
-  "이용약관",
-  "개인정보 처리방침",
-  "쿠키 정책",
-  "접근성",
-  "광고 정보",
-  "블로그",
-  "상태",
-  "채용",
-  "브랜드 리소스",
-  "광고",
-  "마케팅",
-  "비즈니스용 PeaNutter",
-  "개발자",
-  "디렉터리",
-  "설정",
-  "© 2023 Wood Corp.",
-];
-
 const LandingPage = () => {
   const goto = useGoto();
   return (
@@ -286,13 +242,7 @@ const LandingPage = () => {
           </AuthForm>
         </Container>
       </Main>
-      <Nav>
-        <NavList>
-          {NavItems.map((item) => (
-            <NavItem key={item}>{item}</NavItem>
-          ))}
-        </NavList>
-      </Nav>
+      <Nav />
     </Wrapper>
   );
 };
