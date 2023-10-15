92cc8da2aedec9de1d7d6d421c62549eb7b521ef
Stilllee <orodae@gmail.com>
2023-10-15 23:07:07 +0900

    ✨ 깃헙로그인 및 회원가입 기능 구현

--- src/components/LandingPage/SocialLoginBox.tsx
+++ src/components/LandingPage/SocialLoginBox.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import Button from "./Button";
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase";
+import useGoto from "../../hooks/useGoto";
 
 const Container = styled.div`
   display: flex;
@@ -18,13 +21,23 @@ const SocialLoginBtn = styled(Button)`
 `;
 
 const SocialLoginBox = () => {
+  const goto = useGoto();
+  const onGithubLogin = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(auth, provider);
+      goto("/home");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Container>
       <SocialLoginBtn aria-label="Google에서 가입하기">
         <FcGoogle />
         Google에서 가입하기
       </SocialLoginBtn>
-      <SocialLoginBtn aria-label="Github에서 가입하기">
+      <SocialLoginBtn onClick={onGithubLogin} aria-label="Github에서 가입하기">
         <FaGithub />
         Github에서 가입하기
       </SocialLoginBtn>
