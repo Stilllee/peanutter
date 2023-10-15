ef086274256a75fcb6aa980c95c013c09355956e
Stilllee <orodae@gmail.com>
2023-10-16 02:36:59 +0900

    🐛 ProtectedRoute 관련 에러 해결

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -8,7 +8,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     return <Navigate to="/" />;
   }
 
-  return children;
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
