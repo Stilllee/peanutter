62a2acf2049f4fe2467a57a6210c17493d8e2a0d
Stilllee <orodae@gmail.com>
2023-10-15 21:37:29 +0900

    ♻️ 랜딩페이지 버튼 컴포넌트 분리

---
+++ src/components/LandingPage/Button.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  cursor: pointer;
+  width: 300px;
+  height: 40px;
+  border-radius: 20px;
+  border: 1px solid ${({ theme }) => theme.lineGray};
+  background-color: white;
+  font-size: 15px;
+  font-weight: 700;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: ${({ theme }) => theme.lightGray};
+    border-color: ${({ theme }) => theme.lightGray};
+  }
+`;
+export default Button;
