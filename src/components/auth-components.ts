9e2d341067ed6c193a5ca55a4c1adf2e24eaaa81
Stilllee <orodae@gmail.com>
2023-10-16 14:54:15 +0900

    💄 비밀번호 변경 페이지 UI

--- src/components/auth-components.ts
+++ src/components/auth-components.ts
@@ -1,34 +1,56 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
+  background-color: red;
   height: 100%;
+  width: 300px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0;
+  justify-content: center;
 `;
 export const Title = styled.h1`
-  font-size: 42px;
+  background-color: green;
+  display: flex;
+  align-items: center;
+  font-size: 31px;
+  font-weight: 700;
 `;
 export const Form = styled.form`
   margin-top: 50px;
   margin-bottom: 10px;
   display: flex;
+  align-items: center;
   flex-direction: column;
   gap: 10px;
   width: 100%;
 `;
 export const Input = styled.input`
   padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.lineGray};
+  width: 300px;
+  height: 56px;
+
   font-size: 16px;
+  &:focus {
+    outline: 2px solid ${({ theme }) => theme.yellow};
+    border-color: ${({ theme }) => theme.yellow};
+  }
   &[type="submit"] {
+    border-radius: 20px;
+    height: 40px;
+    font-weight: 700;
+    color: white;
     cursor: pointer;
+    background-color: ${({ theme }) => theme.brown};
+    transition: all 0.3s ease;
     &:hover {
-      opacity: 0.8;
+      opacity: 0.9;
+    }
+    &:focus {
+      outline: 2px solid ${({ theme }) => theme.lineGray};
+      border-color: ${({ theme }) => theme.lineGray};
+      opacity: 0.9;
     }
   }
 `;
@@ -40,7 +62,10 @@ export const Error = styled.span`
 export const Switcher = styled.span`
   margin-top: 20px;
   a {
-    color: #1d9bf0;
+    color: ${({ theme }) => theme.hoverYellow};
     text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
   }
 `;
--- src/routes/PasswordReset.tsx
+++ src/routes/PasswordReset.tsx
@@ -1,5 +1,87 @@
+import styled from "styled-components";
+import { TbBrandPeanut } from "react-icons/tb";
+import { Input, Title } from "../components/auth-components";
+import { useState } from "react";
+import CloseModalButton from "../components/common/Modal/CloseModalButton";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: 40px 60px 0 60px;
+`;
+
+const Logo = styled(TbBrandPeanut)`
+  margin: 0 10px;
+`;
+
+const Text = styled.p`
+  font-size: 15px;
+  margin: 8px 0 33px 0;
+  color: ${({ theme }) => theme.darkGray};
+`;
+
+const ResetPwForm = styled.form``;
+
+const EmailInput = styled(Input)`
+  width: 100%;
+`;
+
+const ResetPwSubmit = styled(Input)`
+  width: 440px;
+  height: 52px !important;
+  border-radius: 30px !important;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 0 60px;
+`;
+
 const PasswordReset = () => {
-  return <>패스워드 리셋</>;
+  const [email, setEmail] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    }
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>
+          내 <Logo />
+          계정 찾기
+        </Title>
+        <Text>비밀번호를 변경하려면 계정에 연결된 이메일을 입력해 주세요.</Text>
+        <ResetPwForm>
+          <EmailInput
+            onChange={onChange}
+            name="email"
+            value={email}
+            placeholder="이메일"
+            type="email"
+            required
+          />
+          <ResetPwSubmit
+            type="submit"
+            value={"비밀번호 재설정 이메일 보내기"}
+          />
+        </ResetPwForm>
+        <CloseModalButton />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default PasswordReset;
