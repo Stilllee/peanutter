8220232d014c137fe22de6cfc7019e7ec2d55797
Stilllee <orodae@gmail.com>
2024-03-26 22:44:08 +0900

    💄 회원가입 UI 구현 및 기타 스타일 수정
    - Form 스타일 구현
    - Header 스타일 구현
    - Home 레이아웃 수정

--- src/components/users/SignupForm.tsx
+++ src/components/users/SignupForm.tsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "firebaseApp";
 import React, { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function SignupForm() {
@@ -8,6 +9,7 @@ export default function SignupForm() {
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isHide, setIsHide] = useState<boolean>(true);
   const nav = useNavigate();
 
   const onSubmit = async (e: any) => {
@@ -28,8 +30,7 @@ export default function SignupForm() {
 
     if (name === "email") {
       setEmail(value);
-      const validRegex =
-        /^[a-zA-Z0-9.!#$&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:\.[a-zA-Z0-9-]+)*$/;
+      const validRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
       if (!value?.match(validRegex)) {
         setError("Please enter a valid email.");
       } else {
@@ -49,41 +50,72 @@ export default function SignupForm() {
     }
   };
 
+  const toggleHide = () => {
+    setIsHide((prev) => !prev);
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const input = e.target;
+    const label = input.nextElementSibling;
+
+    if (input.value.length > 0) {
+      label?.classList.add("label--focused");
+    } else {
+      label?.classList.remove("label--focused");
+    }
+  };
+
   return (
-    <form className="form form--lg" onSubmit={onSubmit}>
-      <div className="form__title">Create your accont</div>
+    <form className="form" onSubmit={onSubmit}>
+      <h1 className="form__title">Create your accont</h1>
       <div className="form__block">
-        <label htmlFor="username">Name</label>
         <input
           type="text"
           id="username"
           name="username"
           value={username}
+          maxLength={10}
           required
+          autoComplete="off"
           onChange={onChange}
+          onBlur={handleBlur}
         />
+        <label htmlFor="username" className="placeholder">
+          Username
+        </label>
       </div>
       <div className="form__block">
-        <label htmlFor="email">Email</label>
         <input
           type="text"
           id="email"
           name="email"
           value={email}
           required
+          autoComplete="off"
           onChange={onChange}
+          onBlur={handleBlur}
         />
+        <label htmlFor="email" className="placeholder">
+          Email Adress
+        </label>
       </div>
       <div className="form__block">
-        <label htmlFor="password">Password</label>
         <input
-          type="text"
+          type={isHide ? "password" : "text"}
           id="password"
           name="password"
           value={password}
           required
+          autoComplete="off"
           onChange={onChange}
+          onBlur={handleBlur}
         />
+        <label htmlFor="password" className="placeholder">
+          Password
+        </label>
+        <button type="button" className="toggle-hide" onClick={toggleHide}>
+          {isHide ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
+        </button>
       </div>
       {error && error.length > 0 && (
         <div className="form__block">
@@ -99,9 +131,11 @@ export default function SignupForm() {
           Create an account
         </button>
       </div>
-      <div className="form__block">
+      <div className="form__block form__info">
         Already have an account?
-        <Link to="/users/login">Sign in</Link>
+        <Link to="/users/login" className="form__link">
+          Sign in
+        </Link>
       </div>
     </form>
   );
