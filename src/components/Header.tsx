007967f43d51a83ee1cd5f5ef673f3fe4fd5f716
Stilllee <orodae@gmail.com>
2024-03-26 22:39:18 +0900

    ✨ Header 컴포넌트

---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import AuthContext from "context/AuthContext";
+import { useContext } from "react";
+import { RiLoginBoxLine, RiLogoutBoxRLine } from "react-icons/ri";
+import { TbBrandPeanut } from "react-icons/tb";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const { user } = useContext(AuthContext);
+  const nav = useNavigate();
+  return (
+    <header className="header">
+      <div className="header__flex">
+        <button
+          type="button"
+          aria-label="Profile"
+          title="Profile"
+          onClick={() => nav("/")}
+        >
+          <div className="menu-btn">
+            <img
+              src="/src/assets/profile.webp"
+              alt="user's profile"
+              className="header__profile-img"
+            />
+          </div>
+        </button>
+        <button
+          type="button"
+          aria-label="Home"
+          title="Home"
+          onClick={() => nav("/search")}
+        >
+          <div className="menu-btn logo">
+            <TbBrandPeanut />
+          </div>
+        </button>
+        <button
+          type="button"
+          aria-label={user === null ? "Login" : "Logout"}
+          title={user === null ? "Login" : "Logout"}
+          onClick={() => nav("/notifications")}
+        >
+          <div className="menu-btn">
+            {user === null ? <RiLoginBoxLine /> : <RiLogoutBoxRLine />}
+          </div>
+        </button>
+      </div>
+    </header>
+  );
+}
