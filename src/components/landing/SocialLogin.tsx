f70edc734d896ca39b7acc3f008207ed10a6905c
Stilllee <orodae@gmail.com>
2024-03-27 16:19:19 +0900

    ✨ 소셜 로그인 구현

--- src/components/landing/SocialLogin.tsx
+++ src/components/landing/SocialLogin.tsx
@@ -1,14 +1,50 @@
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+import { app } from "firebaseApp";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 export default function SocialLogin() {
+  const onClickSocailLogin = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    const auth = getAuth(app);
+
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    }
+
+    if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+
+    await signInWithPopup(
+      auth,
+      provider as GoogleAuthProvider | GithubAuthProvider
+    )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorMessage = error?.message;
+        toast(errorMessage);
+      });
+  };
   return (
     <div className="social-login">
-      <button>
+      <button type="button" name="google" onClick={onClickSocailLogin}>
         <FcGoogle />
         Sign up with Google
       </button>
-      <button>
+      <button type="button" name="github" onClick={onClickSocailLogin}>
         <FaGithub />
         Sign up with Github
       </button>
