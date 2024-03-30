4f1f4096a2a6922a69c7d4a42b6f1a7d20b1e0f9
Stilllee <orodae@gmail.com>
2024-03-31 01:35:58 +0900

    ✨ Loader 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Layout from "components/Layout";
+import Loader from "components/Loader";
 import Router from "components/Router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "firebaseApp";
 import { useEffect, useState } from "react";
-import { ClipLoader } from "react-spinners";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -36,13 +36,7 @@ function App() {
         closeButton={false}
         theme="light"
       />
-      {init ? (
-        <Router isAuthenticated={isAuthenticated} />
-      ) : (
-        <div className="clip-loader">
-          <ClipLoader color="#f9d142" />
-        </div>
-      )}
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+import { ClipLoader } from "react-spinners";
+
+export default function Loader() {
+  return (
+    <div className="clip-loader">
+      <ClipLoader color="#f9d142" />
+    </div>
+  );
+}
