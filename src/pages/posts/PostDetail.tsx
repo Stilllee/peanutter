5f1a8d0956c1c74340af744f1bd7d5aab733134f
Stilllee <orodae@gmail.com>
2024-03-31 01:48:18 +0900

    ✨ 게시글 상세페이지 구현

--- src/pages/posts/PostDetail.tsx
+++ src/pages/posts/PostDetail.tsx
@@ -1,8 +1,51 @@
+import Loader from "components/Loader";
+import PostBox from "components/posts/PostBox";
+import { doc, getDoc } from "firebase/firestore";
+import { PostProps } from "pages/home/Home";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { db } from "firebaseApp";
+import Header from "components/Header";
+import { HiArrowLeft } from "react-icons/hi";
+
 export default function PostDetail() {
+  const params = useParams();
+  const nav = useNavigate();
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, "posts", params.id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    if (params.id) {
+      getPost();
+    }
+  }, [getPost, params.id]);
+
   return (
-    <div>
-      Post Detail
+    <div className="post">
+      <Header
+        leftChild={
+          <button
+            type="button"
+            aria-label="Back"
+            title="Back"
+            onClick={() => nav("/")}
+          >
+            <div className="menu-btn">
+              <HiArrowLeft />
+            </div>
+          </button>
+        }
+        centerChild={"Post"}
+      />
+      {post ? <PostBox post={post} /> : <Loader />}
     </div>
   );
 }
-
