001c269e7a442ff4f655123395e78e5c1f1dc4df
Stilllee <orodae@gmail.com>
2024-03-31 01:47:53 +0900

    ✨ 수정페이지에 헤더 추가

--- src/pages/posts/PostEdit.tsx
+++ src/pages/posts/PostEdit.tsx
@@ -1,5 +1,28 @@
+import Header from "components/Header";
 import PostEditForm from "components/posts/PostEditForm";
+import { HiArrowLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 export default function PostEdit() {
-  return <PostEditForm />;
+  const nav = useNavigate();
+  return (
+    <>
+      <Header
+        leftChild={
+          <button
+            type="button"
+            aria-label="Back"
+            title="Back"
+            onClick={() => nav("/")}
+          >
+            <div className="menu-btn">
+              <HiArrowLeft />
+            </div>
+          </button>
+        }
+        centerChild={"Edit"}
+      />
+      <PostEditForm />
+    </>
+  );
 }
