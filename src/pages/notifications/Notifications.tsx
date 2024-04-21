ef05fb2c26bf604383cae3f150bc5cc4a45f8a5b
Stilllee <orodae@gmail.com>
2024-04-21 23:37:12 +0900

    ✨ 알림페이지 헤더 추가

--- src/pages/notifications/Notifications.tsx
+++ src/pages/notifications/Notifications.tsx
@@ -1,3 +1,34 @@
+import Header from "components/Header";
+import AuthContext from "context/AuthContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+const PROFILE_DEFAULT_URL = "/src/assets/profile.webp";
+
 export default function Notifications() {
-  return <div>Notifications</div>;
+  const { user } = useContext(AuthContext);
+  const nav = useNavigate();
+  return (
+    <>
+      <Header
+        leftChild={
+          <button
+            type="button"
+            aria-label="Profile"
+            title="Profile"
+            onClick={() => nav("/profile")}
+          >
+            <div className="menu-btn">
+              <img
+                src={user?.photoURL || PROFILE_DEFAULT_URL}
+                alt="user's profile"
+                className="header__profile-img"
+              />
+            </div>
+          </button>
+        }
+        centerChild={"Notifications"}
+      />
+    </>
+  );
 }
