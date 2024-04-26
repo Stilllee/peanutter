1d21ff6469960e59232954fc0bfc560c61edae4d
Stilllee <orodae@gmail.com>
2024-04-26 18:16:53 +0900

    ✨💄 비밀번호 찾기 폼 뒤로가기 버튼 추가

--- src/pages/users/ResetPassword.tsx
+++ src/pages/users/ResetPassword.tsx
@@ -1,5 +1,27 @@
 import ResetPasswordForm from "components/users/ResetPasswordForm";
+import useTranslation from "hooks/useTranslation";
+import { HiArrowLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 export default function ResetPassword() {
-  return <ResetPasswordForm />;
+  const nav = useNavigate();
+  const translate = useTranslation();
+
+  return (
+    <>
+      <div className="form__btn-back">
+        <button
+          type="button"
+          aria-label={translate("HEADER_BACK")}
+          title={translate("HEADER_BACK")}
+          onClick={() => nav(-1)}
+        >
+          <div className="menu-btn">
+            <HiArrowLeft />
+          </div>
+        </button>
+      </div>
+      <ResetPasswordForm />
+    </>
+  );
 }
